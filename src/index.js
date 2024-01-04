d17e5f6992b8c2cc8173e90f5c0417cdc4f53fc9
mrinalsinha2211 <145749474+mrinalsinha2211@users.noreply.github.com>
2024-01-04 09:42:53 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,15 @@ const fileContent = "Newton School";
 const writeFile = async (fileName, fileContent) => {
   // write code here
   // dont change function name
+
+  try{
+ const file=await fs.appendFile('myfile.txt',"Newton School");
+ console.log(file);
+ return file;
+  }catch(error){
+  console.log(`error in create a file: ${error.message}`);
+  return null;
+  }
 };
 
 module.exports = { writeFile };
